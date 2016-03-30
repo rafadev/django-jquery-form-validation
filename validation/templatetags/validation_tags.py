6d6ea67194d46fb5fb20b3bf101237ad7713c0f3
Madra David <madradavid@gmail.com>
2016-03-30 18:42:46 +0300

    modified validation_tags.py to work with updated versions of Django

--- validation/templatetags/validation_tags.py
+++ validation/templatetags/validation_tags.py
@@ -3,6 +3,9 @@ from django import forms
 from django.core import validators
 from django.utils.encoding import force_unicode
 from json import JSONEncoder
+from django.utils.translation import ugettext as _
+from django.utils.safestring import mark_safe
+
 
 @register.simple_tag
 def validation_rules(form):
@@ -16,47 +19,51 @@ def validation_rules(form):
     """
     if not isinstance(form, forms.BaseForm):
         raise TypeError(_("Expected Django Form instance"))
-    
+
     rules = {}
-    
+
     for field_name in form.fields:
         field_rules = {}
-        
+
         # Required
         if hasattr(form.fields[field_name], 'required'):
             if form.fields[field_name].required == True:
                 field_rules['required'] = True
-        
+
         if(isinstance(form.fields[field_name], forms.TimeField)):
             field_rules['time'] = True
-        
+
         if(isinstance(form.fields[field_name], forms.DateField)):
             field_rules['date'] = True
-        
+
         for validator in form.fields[field_name].validators:
             # Min length
             if(isinstance(validator, validators.MinLengthValidator)):
                 if form.fields[field_name].min_length != None:
-                    field_rules['minlength'] = form.fields[field_name].min_length
+                    field_rules['minlength'] = form.fields[
+                        field_name].min_length
 
             # Max length
             if(isinstance(validator, validators.MaxLengthValidator)):
                 if form.fields[field_name].max_length != None:
-                    field_rules['maxlength'] = form.fields[field_name].max_length
-            
+                    field_rules['maxlength'] = form.fields[
+                        field_name].max_length
+
             # Regular expression
             # Email fields have a regex validator, in this case, we want to use the built-in
             # regex validator in jquery validate, and not Django's regex
             if(isinstance(validator, validators.RegexValidator) and not isinstance(form.fields[field_name], forms.EmailField)):
                 field_rules['regexp'] = validator.regex.pattern
-            
+
             # Email
             if(isinstance(validator, validators.EmailValidator)):
                 field_rules['email'] = True
-        
+
         rules[field_name] = field_rules
-    
-    return JSONEncoder().encode(rules)
+
+    rules = JSONEncoder().encode(rules)
+    return mark_safe(rules)
+
 
 @register.simple_tag
 def validation_messages(form):
@@ -70,42 +77,50 @@ def validation_messages(form):
     """
     if not isinstance(form, forms.BaseForm):
         raise TypeError(_("Expected Django Form instance"))
-    
+
     messages = {}
-    
+
     for field_name in form.fields:
         field_messages = {}
-        
+
         # Required
         if hasattr(form.fields[field_name], 'required'):
             if form.fields[field_name].required == True:
-                field_messages['required'] = force_unicode(form.fields[field_name].error_messages.get('required'))
-        
+                field_messages['required'] = force_unicode(
+                    form.fields[field_name].error_messages.get('required'))
+
         if(isinstance(form.fields[field_name], forms.TimeField)):
-            field_messages['time'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
-        
+            field_messages['time'] = force_unicode(
+                form.fields[field_name].error_messages.get('invalid'))
+
         if(isinstance(form.fields[field_name], forms.DateField)):
-            field_messages['date'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
-        
+            field_messages['date'] = force_unicode(
+                form.fields[field_name].error_messages.get('invalid'))
+
         for validator in form.fields[field_name].validators:
             # Min length
             if(isinstance(validator, validators.MinLengthValidator)):
                 if form.fields[field_name].min_length != None:
-                    field_messages['min_length'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
-            
+                    field_messages['min_length'] = force_unicode(
+                        form.fields[field_name].error_messages.get('invalid'))
+
             # Max length
             if(isinstance(validator, validators.MaxLengthValidator)):
                 if form.fields[field_name].max_length != None:
-                    field_messages['max_length'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
-            
+                    field_messages['max_length'] = force_unicode(
+                        form.fields[field_name].error_messages.get('invalid'))
+
             # Regular expression
             if(isinstance(validator, validators.RegexValidator) and not isinstance(form.fields[field_name], forms.EmailField)):
-                field_messages['regexp'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
-            
+                field_messages['regexp'] = force_unicode(
+                    form.fields[field_name].error_messages.get('invalid'))
+
             # Email
             if(isinstance(validator, validators.EmailValidator)):
-                field_messages['email'] = force_unicode(form.fields[field_name].error_messages.get('invalid'))
+                field_messages['email'] = force_unicode(
+                    form.fields[field_name].error_messages.get('invalid'))
 
         messages[field_name] = field_messages
-    
-    return JSONEncoder().encode(messages)
+
+    messages = JSONEncoder().encode(messages)
+    return mark_safe(messages)
